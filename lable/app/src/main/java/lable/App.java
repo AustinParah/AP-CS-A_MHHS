bdc090792ac8a07a0f1ad5239df125d9f1234cee
AustinParah <parahaustin@gmail.com>
2024-04-10 13:11:42 -0500

    added Lable!
    added the code "Lable" to repo

---
+++ lable/app/src/main/java/lable/App.java
@@ -0,0 +1,60 @@
+// Written by Austin Parah - April 3, 2024
+
+package lable;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class App {
+
+  static File f = null;
+  static FileWriter fw = null;
+  static PrintWriter pw = null;
+  static Scanner SCN = null;
+
+  public static void main(String[] args) {
+    try {
+      fw = new FileWriter("./src/main/java/lables.txt");
+      pw = new PrintWriter(fw, true);
+      f = new File("./src/main/java/lable/addresses.txt");
+      SCN = new Scanner(f);
+    } catch (FileNotFoundException e) {
+      System.out.println("File Not Found");
+      return;
+    } catch(IOException e){
+      System.out.println("Generic IO exeption");
+    }
+
+    while (SCN.hasNextLine()) { // print lables
+      onLine(SCN.nextLine());
+    }
+
+  }
+
+  public static void initSacnner() {
+    try {
+      f = new File("./main/java/lable/addresses.txt");
+      SCN = new Scanner(f);
+    } catch (FileNotFoundException e) {
+      System.out.println("File Not Found");
+    }
+  }
+
+  public static void onLine(String line) {
+    for(String content : line.split("  ")){
+      if(!content.equals("")){ 
+        if(content.startsWith(" ")){
+          content = content.substring(1);
+        }
+        System.out.println(content);
+        pw.println(content);
+      }
+    }
+    System.out.println();
+    pw.println("\n");
+  }
+}
